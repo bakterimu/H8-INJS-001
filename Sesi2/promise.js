c04869744f2419123b2d45ec6047958fdc07292b
izharmayrnd <mayrandai12@gmail.com>
2021-09-02 23:37:13 +0700

    revisi pemakaian callback

--- Sesi2/promise.js
+++ Sesi2/promise.js
@@ -1,17 +1,16 @@
 let fs = require('fs')
 
-fs.readFile('./Sesi2/input.txt',(err,data) => {
+fs.readFile('./Sesi2/input.txt','utf-8',(err,data) => {
   if (err) {
     console.log(err)
   } else {
     console.log(data)
-  }
-})
-
-fs.readFile('./Sesi2/inputt.txt',(err,data) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(toString(data))
+    fs.readFile('./Sesi2/input2.txt','utf-8',(err,data) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(data)
+      }
+    })
   }
 })
