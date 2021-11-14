b9d17446de4763bea897a21ab36839918851b392
izharmayrnd <mayrandai12@gmail.com>
2021-11-15 04:07:37 +0700

    menambahkan sandbox.js di sesi 15

--- sesi15-debugging/sandbox.js
+++ sesi15-debugging/sandbox.js
@@ -1,13 +1,31 @@
-// Pengecekan bilangan prima untuk 13
+// // Pengecekan bilangan prima untuk 13
 
-isPrime = (n) => {
-  for(let i = 2; i <= Math.sqrt(n); i++){
-    if (n % i == 0) return false;
-  }
-  return true;
+// isPrime = (n) => {
+//   for(let i = 2; i <= Math.sqrt(n); i++){
+//     if (n % i == 0) return false;
+//   }
+//   return true;
+// }
+//   if (isPrime(23)) {
+//     console.log('Bilangan 23 Prima');
+//   } else {
+//     console.log('Bilangan 23 bukan prima');
+//   }
+
+
+function satu () {
+  dua()
 }
-  if (isPrime(23)) {
-    console.log('Bilangan 23 Prima');
-  } else {
-    console.log('Bilangan 23 bukan prima');
-  }
+
+function dua() {
+  tiga()
+}
+
+function tiga() {
+  throw new Error("Ini pesan error")
+}
+
+for(let i = 0; i < 1000000; i++) {
+  satu()
+  axios()
+}
